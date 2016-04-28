5b5c38c5d97ac3814211e02473bba08bc669c0b8
Kyle <thorn129@mail.chapman.edu>
2016-04-27 17:46:51 -0700

    Merging from branch

--- Assignment 4/EmailViewController.swift
+++ Assignment 4/EmailViewController.swift
@@ -1,27 +1,26 @@
 //
-//  DataTableViewController.swift
+//  Email.swift
 //  Assignment 4
 //
-//  Created by Kyle Thornton on 4/17/16.
+//  Created by Kyle Thornton on 4/27/16.
 //  Copyright © 2016 Thorn129. All rights reserved.
 //
 
 import UIKit
 
-class DataTableViewController: UIViewController {
-
+class EmailViewController: UIViewController {
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
 }
