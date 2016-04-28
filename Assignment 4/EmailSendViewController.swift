df04ba24310a705e9079270d454d168c35849f7e
Kyle <thorn129@mail.chapman.edu>
2016-04-27 18:17:24 -0700

    added email

---
+++ Assignment 4/EmailSendViewController.swift
@@ -0,0 +1,27 @@
+//
+//  EmailSendViewController.swift
+//  Assignment 4
+//
+//  Created by Kyle Thornton on 4/27/16.
+//  Copyright © 2016 Thorn129. All rights reserved.
+//
+
+import UIKit
+
+class EmailSendViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
+
